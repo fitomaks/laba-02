947f86ba4edfe74b88346065a33dfe21992a2c3f
maks <a1@MacBook-Air-1.local>
2022-03-29 17:15:06 +0300

    obnivil

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
-	cout << "hello world!!" << endl;
+	cout << "vvedite imy polzovately  "<< endl;
+	string name;
+	cin >> name ;
+	cout << "hello world!!" << name << endl;
 	return 1;
 }
  
