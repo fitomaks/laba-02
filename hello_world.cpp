74f33bae0b71b16555b9490d8fc199e651f3e283
maks <a1@MacBook-Air-1.local>
2022-03-30 16:08:07 +0300

    sreens

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <string>
 
-<<<<<<< HEAD
-
 int
 main()
 {
@@ -11,14 +9,4 @@ main()
 	std::cin >> name;
 	std::cout << "hello world!!" << name << std::endl; // vivod -  вывод 
 	return 1;
-=======
-int
-main()
-{
-  std::cout << "vvedite imy polzovately  " << std::endl; // zapros imeni
-  std::string name;
-  std::cin >> name;
-  std::cout << "hello world!!" << name << std::endl; // vivod
-  return 1;
->>>>>>> clang-format
 }
