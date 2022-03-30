ce81fc6bef970df57d91aca06c8225ac47afc30d
maks <a1@MacBook-Air-1.local>
2022-03-30 13:20:00 +0300

    clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include <string>
 
+<<<<<<< HEAD
 
-int main(){
-	std::cout << "vvedite imy polzovately  "<< std::endl;//zapros imeni - запрос имени 
+int
+main()
+{
+	std::cout << "vvedite imy polzovately  "<< std::endl; // zapros imeni - запрос имени 
 	std::string name;
-	std::cin >> name ;
-	std::cout << "hello world!!" << name << std::endl;// vivod -  вывод 
+	std::cin >> name;
+	std::cout << "hello world!!" << name << std::endl; // vivod -  вывод 
 	return 1;
+=======
+int
+main()
+{
+  std::cout << "vvedite imy polzovately  " << std::endl; // zapros imeni
+  std::string name;
+  std::cin >> name;
+  std::cout << "hello world!!" << name << std::endl; // vivod
+  return 1;
+>>>>>>> clang-format
 }
- 
-
