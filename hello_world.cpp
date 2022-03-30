b520d7ae60e5bedeef891f91707733dee37bbc18
fitomaks <100668619+fitomaks@users.noreply.github.com>
2022-03-30 13:27:03 +0300

    Russian comment

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,10 +3,10 @@
 
 
 int main(){
-	std::cout << "vvedite imy polzovately  "<< std::endl;//zapros imeni
+	std::cout << "vvedite imy polzovately  "<< std::endl;//zapros imeni - запрос имени 
 	std::string name;
 	std::cin >> name ;
-	std::cout << "hello world!!" << name << std::endl;// vivod
+	std::cout << "hello world!!" << name << std::endl;// vivod -  вывод 
 	return 1;
 }
  
